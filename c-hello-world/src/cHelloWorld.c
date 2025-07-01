946a785ce29b9168151cf41cbc12bdc9407a4b17
sjharb <zzmharbison@gmail.com>
2025-07-01 10:29:32 -0500

    Added hello world c example project.

---
+++ c-hello-world/src/cHelloWorld.c
@@ -0,0 +1,11 @@
+// c program hello world example
+#include <stdio.h>
+
+// Main function: i.e. entry point for execution of the c program
+int main() {
+    // print hellow world to the console or command line
+    printf("Hello World!");
+
+    // return 0 meaning successful program execution
+    return 0;
+}
